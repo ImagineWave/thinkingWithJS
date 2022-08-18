c83d54a069d1cd6f5761072c3b4ef0f51dedef5a
Кукавский Владимир Витальевич <vkukavsky@blogic.ru>
2022-08-18 18:56:36 +0500

    3 Практика

---
+++ todo.js
@@ -0,0 +1,8 @@
+"use strict";
+const changeButton = document.getElementsById("change");
+const addButton = document.getElementsById("add");
+const removeButton = document.getElementsById("remove");
+const upButton = document.getElementsById("up");
+const downButton = document.getElementsById("down");
+
+const todoList = document.getElementById();
